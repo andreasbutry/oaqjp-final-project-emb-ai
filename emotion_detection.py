07bcb5247464e1e0a5d354bdd6385c1a9f02ac52
Andreas Butry <andreas.butry@googlemail.com>
2025-11-07 07:25:14 -0500

    dominant emotion

--- emotion_detection.py
+++ emotion_detection.py
@@ -16,4 +16,26 @@ def emotion_detector(text_to_analyze):
     # Parsing the JSON response from the API
     formatted_response = json.loads(response.text)
 
-    return formatted_response
+    emotions = formatted_response['emotionPredictions'][0]['emotion']
+
+    #  Make sure, that all keys are available
+    anger = emotions.get('anger', 0)
+    disgust = emotions.get('disgust', 0)
+    fear = emotions.get('fear', 0)
+    joy = emotions.get('joy', 0)
+    sadness = emotions.get('sadness', 0)
+    
+    emotions_dict = {
+        'anger': anger,
+        'disgust': disgust,
+        'fear': fear,
+        'joy': joy,
+        'sadness': sadness
+    }
+    # Find dominant emotion by max score
+    dominant_emotion = max(emotions_dict, key=emotions_dict.get)
+    
+    # Add dominant emotion
+    emotions_dict['dominant_emotion'] = dominant_emotion
+
+    return emotions_dict
